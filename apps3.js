2b0cd8c6c78054587162caf96601a064b9b12ffe
Greg <Gdixon5125@gmail.com>
2021-06-16 15:26:52 -0400

    M,M

--- apps3.js
+++ apps3.js
@@ -3,11 +3,14 @@
 for (i = 0; i < 11; i++) {
     text += i + "<br>";}
 //3) Write a while loop with true as the conditional WARNING: This will cause an infinite loop condition so be mindful that you don't save your file if you 're using live server, otherwise don't execute your JS until you complete step 5
-
+while (true) {
+    console.log(true);
+    break;
+}
 //4) In the code block of your while loop write a break statement to terminate the loop NOTE: it is ok to save or execute your JavaScript now
 
 //5) Write a for loop that starts at 99 and DECREMENTS down to 1
-for (i = 99; i < 99; i--) {
+for (i = 99; i > 0; i--) {
     text += i + "<br>";}
 //6) In the code block of the for loop console log the concatenation of your iterator (typically "i") and " bottles of beer on the wall. Take one down pass it around." NOTE: should write "99 bottles of beer on the wall. Take one down pass it around." => "1 bottles of beer on the wall. Take one down pass it around." BONUS: Format it to where it is "1 bottle" instead of "1 bottles"
 
@@ -17,5 +20,5 @@ var bools = [true, true, false, true, false, true, true, true];
 for()
 //9) Create a variable named str set it to the string "apples"
 var str = "apples";
-/*) Loop through each character of the str string. Console log all the vowel characters. NOTE: a, e, i, o, u 
+/*10) Loop through each character of the str string. Console log all the vowel characters. NOTE: a, e, i, o, u 
 HINT: str.length, use an if statement to check it the character is a vowel, if so log it to the console
