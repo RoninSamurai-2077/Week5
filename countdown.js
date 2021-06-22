9c1423484682239d69d27aadc41414187ef4845e
Greg <Gdixon5125@gmail.com>
2021-06-22 08:43:05 -0400

    Week 5 Homework 4 - Countdown

---
+++ countdown.js
@@ -0,0 +1,11 @@
+//1) In your week5 repo and on your practice branch, add a countdown.html, a countdown.js, and optionally a countdown.css.
+
+//2) You will be creating a countdown timer to a certain time and date (Halloween, your birthday, release of a new movie, or let your user pick a date and time if you want to be real fancy).
+
+//3) The increments should show on screen. It should stop when it reaches the date and not go into the negatives.
+
+//4) test test test test.
+
+//5) optional: style it.
+
+//6) git add, commit, push, merge
